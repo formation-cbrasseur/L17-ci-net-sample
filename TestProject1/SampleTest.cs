2720440018b5b3b5c0e46499ce59fdbbdb92db68
Cédric BRASSEUR <cdric.brasseur@gmail.com>
2025-06-13 11:11:06 +0200

    CI : Add simple .net CI

---
+++ TestProject1/SampleTest.cs
@@ -0,0 +1,12 @@
+namespace TestProject1
+{
+    [TestClass]
+    public class SampleTest
+    {
+        [TestMethod]
+        public void FakeTest_Simple_AssertTrueIsTrue()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
